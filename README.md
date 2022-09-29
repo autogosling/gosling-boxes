ee85308f197e79432f8ddc9d6e3b216e28e1479a
wangqianwen0418 <wangqianwen0418@163.com>
2022-09-29 13:43:50 -0400

    feat: puppeteer to get api info

---
+++ README.md
@@ -0,0 +1,19 @@
+ 
+# Gosling-Boxes
+
+First, make sure you have installed node.js in your computer.
+
+### how to run the node.js script
+
+In the root folder (gosling-boxes),  
+run `npm install`
+
+Inside the script folder (gosling-boxes/scripts), run
+`node gosling-boxes.js example.json tracks.json`  
+example.json stores the visualization spec.  
+tracks.json saves the outputs of api calls.  
+
+
+### how to run the react app
+`npm install`
+`npm start`
---
+++ scripts/gosling-boxes.js
@@ -0,0 +1,68 @@
+import puppeteer from "puppeteer";
+import * as fs from "node:fs/promises";
+
+/**
+ * @param {string} spec
+ * @param {{ reactVersion: string, pixijsVersion: string, higlassVersion: string, goslingVersion: string }} pkgOptions
+ * @returns {string}
+ */
+function html(spec, {
+    reactVersion = "17",
+    pixijsVersion = "6",
+    higlassVersion = "1.11",
+    goslingVersion = "0.9.22",
+} = {}) {
+    let baseUrl = "https://unpkg.com";
+    return `\
+<!DOCTYPE html>
+<html>
+	<link rel="stylesheet" href="${baseUrl}/higlass@${higlassVersion}/dist/hglib.css">
+	<script src="${baseUrl}/react@${reactVersion}/umd/react.production.min.js"></script>
+	<script src="${baseUrl}/react-dom@${reactVersion}/umd/react-dom.production.min.js"></script>
+	<script src="${baseUrl}/pixi.js@${pixijsVersion}/dist/browser/pixi.min.js"></script>
+	<script src="${baseUrl}/higlass@${higlassVersion}/dist/hglib.js"></script>
+	<script src="${baseUrl}/gosling.js@${goslingVersion}/dist/gosling.js"></script>
+<body>
+	<div id="vis"></div>
+	<script>
+		let api = gosling.embed(document.getElementById("vis"), JSON.parse(\`${spec}\`))
+		window.tracks = api.then(a=>a.getTracks())
+	</script>
+</body>
+</html>`;
+}
+
+
+/**
+ * @param {string} spec
+ * @param {string} apiName
+ * @returns {Promise<Buffer>}
+ */
+async function callAPI(spec, output) {
+    let browser = await puppeteer.launch({
+        headless: true,
+        args: ["--use-gl=swiftshader"], // more consistent rendering of transparent elements
+    });
+
+    let page = await browser.newPage();
+    await page.setContent(html(spec), { waitUntil: "networkidle0" });
+    await page.waitForSelector(".gosling-component");
+
+    let trackInfos = await page.evaluate(() => tracks)
+    fs.writeFile(output, JSON.stringify(trackInfos.map(d => d['shape'])))
+
+    await browser.close();
+}
+
+let input = process.argv[2];
+let output = process.argv[3];
+
+if (!input || !output) {
+    console.error(
+        "Usage: node gosling-screenshot.js <input.json> <output.{png,jpeg,webp}>",
+    );
+    process.exit(1);
+}
+
+let spec = await fs.readFile(input, "utf8");
+await callAPI(spec, output);
